ec2edb9be9ad790e42f9274ecdb70e0fe64629e1
LeonardoHMS <leorique2010@hotmail.com>
2022-12-12 23:02:51 -0300

    Removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
     return render(request, 'recipes/home.html', context={
         'name': 'Leonardo Mantovani',
     })
-
-
-def contato(request):
-    return render(request, 'me-apague/temp.html')
-
-
-def sobre(request):
-    return HttpResponse('SOBRE')
