ee598a37c212d22746f57c66c076aa919f77635c
LeonardoHMS <leorique2010@hotmail.com>
2022-12-15 22:38:19 -0300

    create recipe-view

--- projeto/urls.py
+++ projeto/urls.py
@@ -20,5 +20,4 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('recipes.urls')),
-    path('recipes/', include('recipes.urls')),  # /
 ]
