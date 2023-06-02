d5bcc3ed51f079db26cb8f171b423531a7b14611
LeonardoHMS <leorique2010@hotmail.com>
2023-06-01 21:03:12 -0300

    Creating Model Tests

--- recipes/tests/test_recipe_model_recipe.py
+++ recipes/tests/test_recipe_model_recipe.py
@@ -1,10 +1,14 @@
+from django.core.exceptions import ValidationError
+
 from .test_recipe_base import RecipeTestBase
 
 
 class RecipeModelTest(RecipeTestBase):
     def setUp(self) -> None:
-        self.recipes = self.make_recipe()
+        self.recipe = self.make_recipe()
         return super().setUp()
 
-    def test_the_test(self):
-        recipe = self.recipes
+    def test_recipe_title_raises_error_if_title_has_more_than_65_chars(self):
+        self.recipe.title = 'A' * 70
+        with self.assertRaises(ValidationError):
+            self.recipe.full_clean()  # Validation
