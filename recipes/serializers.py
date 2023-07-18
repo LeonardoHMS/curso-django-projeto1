9dba440bb81a46fe13cc93afaa7d09f56d656631
LeonardoHMS <leorique2010@hotmail.com>
2023-07-18 12:31:05 -0300

    creating news apis

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from authors.validators import AuthorRecipeValidator
 from tag.models import Tag
 
 from .models import Recipe
@@ -18,6 +19,9 @@ class RecipeSerializer(serializers.ModelSerializer):
             'id', 'title', 'description', 'author',
             'preparation', 'category', 'public',
             'tags', 'tag_objects', 'tag_links',
+            'preparation_time', 'preparation_time_unit',
+            'servings', 'servigs_unit',
+            'preparation_steps', 'cover'
         ]
     public = serializers.BooleanField(
         source='is_published',
@@ -38,9 +42,38 @@ class RecipeSerializer(serializers.ModelSerializer):
     tag_links = serializers.HyperlinkedRelatedField(
         many=True,
         source='tags',
-        queryset=Tag.objects.all(),
-        view_name='recipes:recipe_api_v2_tag'
+        view_name='recipes:recipe_api_v2_tag',
+        read_only=True,
     )
 
     def get_preparation(self, recipe):
         return f'{recipe.preparation_time} {recipe.preparation_time_unit}'
+
+    def validate(self, attrs):
+        if self.instance is not None and attrs.get('title') is None:
+            attrs['title'] = self.instance.title
+
+        if self.instance is not None and attrs.get('description') is None:
+            attrs['description'] = self.instance.description
+
+        if self.instance is not None and attrs.get('servings') is None:
+            attrs['servings'] = self.instance.servings
+
+        if self.instance is not None and attrs.get('preparation_time') is None:
+            attrs['preparation_time'] = self.instance.preparation_time
+
+        super_validate = super().validate(attrs)
+        AuthorRecipeValidator(
+            data=attrs,
+            ErrorClass=serializers.ValidationError
+        )
+        return super_validate
+
+    def save(self, **kwargs):
+        return super().save(**kwargs)
+
+    def create(self, validated_data):
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        return super().update(instance, validated_data)
